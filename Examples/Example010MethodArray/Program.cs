41eda1d17325fafa00634d2b7df4894c79106b6c
«SERGEY» <szely@yandex>
2022-09-11 19:04:40 +0400

    Lessons

---
+++ Examples/Example010MethodArray/Program.cs
@@ -0,0 +1,13 @@
+﻿int [] array = {1,2,3,12,5,4,7,4};
+int n = array.Length;
+int find = 4;
+int index = 0;
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
