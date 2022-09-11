93446f4e15b0ff7fa9bfc0426ce0983e6bf479c1
«SERGEY» <szely@yandex>
2022-09-11 19:08:56 +0400

    Practies_2

---
+++ Task/MyMethods/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using static System.Console;
+namespace MyMethods;
+public class MyExample
+{
+   public static int[,] GetMatrixArray(int rows, int columns, int minValue, int maxValue)
+{
+    int[,] result = new int[rows, columns];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return result;
+}
+
+public static void PrintMatrixArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Write($"{inArray[i, j]} ");
+        }
+        WriteLine();
+    }
+}
+}
